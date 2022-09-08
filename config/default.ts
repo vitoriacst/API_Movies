e42235a49f159c55e7adb17e13d6f321d56f9af3
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-09-07 22:30:02 -0300

    perf: :wrench: configuring Winston

--- config/default.ts
+++ config/default.ts
@@ -4,4 +4,5 @@ const dbPassword = process.env.DB_PASS;
 export default {
   port: 3000,
   dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.35glf5f.mongodb.net/?retryWrites=true&w=majority`,
+  env:"development",
 }
