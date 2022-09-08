d9cd7211b96bb7f048cd9e701fba1317dc7fee7a
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-09-07 21:15:22 -0300

    perf: :construction: defining the port and performing the imports

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('ok');
+import express  from "express";
+import config from "config";
+
+const app = express()
+
+app.use(express.json())
+
+const port = config.get<number>("port");
+
+app.listen(3000, async ()=>{
+  console.log(`The application is running on port ${port}`);
+})
