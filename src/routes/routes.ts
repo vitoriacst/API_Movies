634bf83d67e48899f50d7b3e24a7c3b6d324e416
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-09-07 23:12:29 -0300

    feat: :sparkles: creating a validation middleware

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,9 @@
-import { Router , Request , Response } from "express";
+import { Request, Response, Router } from "express";
 import { createMovie } from "../controllers/movieControllers";
+import { validate } from '../middleware/handleValidation';
 
 const router = Router()
 
 export default router.get("/test", (req: Request , res: Response)=>{
   res.status(200).send("ok!")
-}).post("/movie" , createMovie)
+}).post("/movie" ,validate, createMovie)
