c4c35cf5f2ad0134947c85ea8492743dc699c629
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-09-08 21:02:55 -0300

    feat: :sparkles: validation

--- src/middleware/movieValidation.ts
+++ src/middleware/movieValidation.ts
@@ -1,2 +1,23 @@
 import { body } from "express-validator";
 
+export const movieCreateValidation = () => {
+  return [
+    body('title')
+      .isString()
+      .withMessage("the title is required")
+      .isLength({min: 5})
+      .withMessage("the title must have 5 characters"),
+    body('rating')
+      .isNumeric()
+      .withMessage("the rating must be a number")
+      .custom((value: number) => {
+        if(value < 0 || value > 0){
+          throw new Error("the rating must be from 0 to 10")
+        }
+        return true;
+      }),
+      body("description").isString().withMessage("the description is mandatory"),
+      body("director").isString().withMessage("the name of the director is mandatory"),
+      body("poster").isURL().withMessage("the image must be a URL")
+  ]
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from "express";
 import { createMovie } from "../controllers/movieControllers";
 import { validate } from '../middleware/handleValidation';
+import { movieCreateValidation } from "../middleware/movieValidation";
 
 const router = Router()
 
 export default router.get("/test", (req: Request , res: Response)=>{
   res.status(200).send("ok!")
-}).post("/movie" ,validate, createMovie)
+}).post("/movie" , movieCreateValidation ,validate, createMovie)
